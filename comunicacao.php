cd4a261a4ad42fb58efe2769c1996f744c7011fa
Anderson Rici Amorim <anderson.riciamorim@gmail.com>
2021-09-09 12:12:08 -0300

    Adicionado tratamento de páginas para BOM ou RUIM

--- comunicacao.php
+++ comunicacao.php
@@ -18,5 +18,69 @@
     $response = curl_exec($ch);
     curl_close($ch);
     
-    echo $response;
-?>
+    $obj = json_decode($response);
+    
+    if($obj->{'sentimento'} == "Bom"){ ?>
+        <!DOCTYPE html>
+        <html>
+
+        <head>
+            <meta charset="utf-8">
+            <title>Analisador de sentimento com Machine Learning</title>
+            <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+        </head>
+
+        <body>
+                <div class="div-conteiner" style="padding: 10px;">
+                    <h1>Sentimento BOM!</h1>
+                    <h6>Que legal! Parece que você gostou do nosso produto!</h6>
+
+                    
+
+                    <div class="form-group">
+                        <button onclick="window.location.href='home.php';" class="btn btn-primary btn-lg">Nova Análise</button>
+                    </div>
+            </form>
+
+            <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+            <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.mask/1.14.15/jquery.mask.min.js"></script>
+            <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+            <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
+            </div>
+        </body>
+
+        </html>
+    <?php 
+    } 
+    else{ ?>
+        <!DOCTYPE html>
+        <html>
+
+        <head>
+            <meta charset="utf-8">
+            <title>Analisador de sentimento com Machine Learning</title>
+            <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+        </head>
+
+        <body>
+                <div class="div-conteiner" style="padding: 10px;">
+                    <h1>Sentimento RUIM!</h1>
+                    <h6>Que pena! Nosso suporte entrará em contato para podermos ajudá-lo!</h6>
+
+                    
+
+                    <div class="form-group">
+                        <button onclick="window.location.href='home.php';" class="btn btn-primary btn-lg">Nova Análise</button>
+                    </div>
+            </form>
+
+            <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+            <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.mask/1.14.15/jquery.mask.min.js"></script>
+            <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+            <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
+            </div>
+        </body>
+
+        </html>
+    <?php } 
+    ?>
