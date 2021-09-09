6459a748facf86353f70b79b75cbece6a26e8798
Anderson Rici Amorim <anderson.riciamorim@gmail.com>
2021-09-09 09:23:13 -0300

    Função de inferencia criada

--- inferencia.py
+++ inferencia.py
@@ -4,8 +4,20 @@ import joblib
 
 def extractor(comentario):
     transformer = joblib.load('transformer.joblib')
-    #print(comentario)
-    #print(type(comentario))
     comentario = transformer.transform([comentario])
-    #print(comentario)
     return comentario
+
+def inferencia(model,X_test,k=1):
+    
+    # get probabilities instead of predicted labels, since we want to collect top 3
+    probs = model.predict_proba(X_test)
+
+    # GET TOP K PREDICTIONS BY PROB - note these are just index
+    best_n = np.argsort(probs, axis=1)[:,-k:]
+    
+    # GET CATEGORY OF PREDICTIONS
+    preds=[[model.classes_[predicted_cat] for predicted_cat in prediction] for prediction in best_n]
+    
+    preds=[ item[::-1] for item in preds]
+    
+    return preds
