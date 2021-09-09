3a264d52788e1bad0a2a67742221ad3d9112ee74
Anderson Rici Amorim <anderson.riciamorim@gmail.com>
2021-09-09 09:30:23 -0300

    Rota para análise de sentimento finalizada

--- main.py
+++ main.py
@@ -1,19 +1,18 @@
 from fastapi import FastAPI
 import joblib
-from inferencia import extractor
+from inferencia import extractor, inferencia
 
 model = joblib.load('modelo.joblib')
 app = FastAPI()
 
-
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
-
 @app.get('/analise_sentimento/')
 async def analisa_comentario(message: str):
-    print('oiiiii')
     vetor = extractor(message)
-    print(vetor)
-    return {"vetor":vetor}
-    #return classify_message(model, message)
+    preds = inferencia(model,vetor,1)
+
+    if preds[0][0] == 0.0:
+        resposta = 'Ruim'
+    else:
+        resposta = 'Bom'
+    
+    return {"sentimento":resposta}
