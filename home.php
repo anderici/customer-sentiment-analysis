a035652cb39fc7d287259c231b87151b63213a49
Anderson Rici Amorim <anderson.riciamorim@gmail.com>
2021-09-09 11:46:24 -0300

    Alterações HTML no home.php

--- home.php
+++ home.php
@@ -3,7 +3,7 @@
 
 <head>
     <meta charset="utf-8">
-    <title>Formulário Bootstrap</title>
+    <title>Analisador de sentimento com Machine Learning</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
 </head>
 
